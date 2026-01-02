444f5925bfcb7500bc85cb6a3d3519f53ef1aa52
José Luis Medina Carretero <joseluismc81@gmail.com>
2026-01-02 19:37:58 +0100

    fix: add plugin repositories to avoid 403 from Maven Central

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://repo.spring.io/milestone") }
+    }
+}
+
 rootProject.name = "real-state-investing-backend"
